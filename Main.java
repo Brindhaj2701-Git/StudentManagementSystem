6517fd854e1046a4cdd0f32dc7b82f62168c3538
Brindhaj2701-Git <brindhaj2701@gmail.com>
2026-07-16 21:17:30 +0530

    Add Student class with constructor and methods

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Student Management System");
+        System.out.println("=== Student Management System ===\n");
+
+        Student student = new Student(
+                101,
+                "Brindha",
+                21,
+                "CSE",
+                92.5
+        );
+
+        student.display();
     }
 }
--- Student.java
+++ Student.java
@@ -1,3 +1,65 @@
-public class Student{
-    
+public class Student {
+
+    // Variables
+    private int id;
+    private String name;
+    private int age;
+    private String department;
+    private double marks;
+
+    // Constructor
+    public Student(int id, String name, int age, String department, double marks) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    // Getter Methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    // Setter Methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    // Display Method
+    public void display() {
+        System.out.println("Student ID : " + id);
+        System.out.println("Name       : " + name);
+        System.out.println("Age        : " + age);
+        System.out.println("Department : " + department);
+        System.out.println("Marks      : " + marks);
+    }
 }
